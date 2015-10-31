e1b2a520e55deaa0cee1408439a8d666e0b6f5d2
Kai Zhang <kylerzhang11@gmail.com>
2015-10-31 16:03:18 +0800

    Split tutorial canvas from main.

--- main.py
+++ main.py
@@ -1,57 +1,46 @@
-from array import array
+import argparse
 import sys
 
-import OpenGL.GL as gl
-# import OpenGL.GLU as glu
 from PyQt4 import QtGui
-from PyQt4 import QtOpenGL
 
-import shader
+import tutorial2
 
 
-class Canvas(QtOpenGL.QGLWidget):
+TUTORIAL_CANVAS = {
+    "tutorial2": tutorial2.Canvas,
+}
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tutorials", nargs="+", help="Tutorials to display",
+                        choices=TUTORIAL_CANVAS.keys())
+    return parser.parse_args()
+
+
+class TutorialList(QtGui.QWidget):
     def __init__(self, parent=None):
-        super(Canvas, self).__init__(parent)
-        self.setFixedSize(800, 600)
+        super(TutorialList, self).__init__(parent)
+        layout = QtGui.QVBoxLayout()
+        self.setLayout(layout)
 
-    def initializeGL(self):
-        gl.glClearColor(1.0,1.0,1.0,0.0)
-        # gl.glColor3f(0.0,0.0, 0.0)
-        # gl.glPointSize(4.0)
-        # gl.glMatrixMode(gl.GL_PROJECTION)
-        # gl.glLoadIdentity()
-        # glu.gluOrtho2D(0.0,640.0,0.0,480.0)
-        gl.glViewport (0, 0, 800, 600)
-        gl.glClearColor (0.0, 0.5, 0.5, 1.0)
-        gl.glEnableClientState (gl.GL_VERTEX_ARRAY)
-
-        vertices = [ 0.0, 1.0, 0.0,  0.0, 0.0, 0.0,  1.0, 1.0, 0.0 ]
-        self._vbo = gl.glGenBuffers (1)
-        ar = array("f", vertices)
-        gl.glBindBuffer (gl.GL_ARRAY_BUFFER, self._vbo)
-        gl.glBufferData (gl.GL_ARRAY_BUFFER, len(vertices)*4,
-                      ar.tostring(), gl.GL_STATIC_DRAW)
-        self._shader_program = shader.LoadShaders("simple_es_shader.vs",
-                                                  "simple_es_shader.ps")
-
-    def paintGL(self):
-        gl.glClear (gl.GL_COLOR_BUFFER_BIT|gl.GL_DEPTH_BUFFER_BIT)
-        gl.glUseProgram(self._shader_program)
-        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self._vbo)
-        gl.glVertexPointer(3, gl.GL_FLOAT, 0, None)
-        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3)
+    def append(self, widget):
+        self.layout().addWidget(widget)
 
 
 class MainWindow(QtGui.QMainWindow):
-    def __init__(self):
+    def __init__(self, tutorials):
         super(MainWindow, self).__init__()
-        canvas = Canvas()
-        self.setCentralWidget(canvas)
+        tutorial_list = TutorialList()
+        self.setCentralWidget(tutorial_list)
+        for each_tutorial in tutorials:
+            tutorial_list.append(TUTORIAL_CANVAS[each_tutorial]())
 
 
 def main():
     app = QtGui.QApplication(["OpenGL Tutorial"])
-    main_window = MainWindow()
+    arguments = parse_arguments()
+    main_window = MainWindow(arguments.tutorials)
     main_window.show()
     sys.exit(app.exec_())
 
